7c8b25aa5473ddc0e6b89957d6a4c87c379d1f46
negibouze <itathirdchildnegi@gmail.com>
2019-04-23 01:05:22 +0900

    first commit
    とりあえず動いた。

---
+++ nuxt.config.ts
@@ -0,0 +1,56 @@
+import NuxtConfiguration from '@nuxt/config'
+
+const config: NuxtConfiguration = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Nuxt+hapi with TypeScript',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt+hapi with TypeScript' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
+
+export default config
